f84a820f336259d390658e0e0ed2492c8cde964d
Karol-Ramirez03 <karol03112006@gmail.com>
2024-10-08 16:29:04 -0500

    feat: :sparkles: init

---
+++ src/main/java/com/servimax/proservicehub/ProservicehubApplication.java
@@ -0,0 +1,13 @@
+package com.servimax.proservicehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProservicehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProservicehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servimax/proservicehub/ProservicehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servimax.proservicehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProservicehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
