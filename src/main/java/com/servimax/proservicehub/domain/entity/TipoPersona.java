72c51db072b5e66f3c2ccbfad41e6ec45c381068
Karol-Ramirez03 <karol03112006@gmail.com>
2024-10-09 12:24:37 -0500

    docs: :sparkles: new Entity person

--- src/main/java/com/servimax/proservicehub/domain/entity/TipoPersona.java
+++ src/main/java/com/servimax/proservicehub/domain/entity/TipoPersona.java
@@ -1,10 +1,13 @@
 package com.servimax.proservicehub.domain.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,4 +20,35 @@ public class TipoPersona {
 
     @Column
     private String nombre;
+
+    @OneToMany(mappedBy = "tipoPersona")
+    private List<Personas> personas;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Personas> getPersonas() {
+        return personas;
+    }
+
+    public void setPersonas(List<Personas> personas) {
+        this.personas = personas;
+    }
+
+    
+
+
 }
--- src/main/java/com/servimax/proservicehub/domain/entity/TipoTelefono.java
+++ src/main/java/com/servimax/proservicehub/domain/entity/TipoTelefono.java
@@ -1,10 +1,13 @@
 package com.servimax.proservicehub.domain.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,4 +21,33 @@ public class TipoTelefono {
     @Column
     private String nombre;
 
+    @OneToMany(mappedBy = "tipoTelefono")
+    private List<TelPersona> telPersona;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<TelPersona> getTelPersona() {
+        return telPersona;
+    }
+
+    public void setTelPersona(List<TelPersona> telPersona) {
+        this.telPersona = telPersona;
+    }
+
+
+    
 }
