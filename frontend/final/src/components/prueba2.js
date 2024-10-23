ff9fbf6ecd5f46085e4952574b11ac3f1af8b0b9
Lauraw100 <158192937+Lauraw100@users.noreply.github.com>
2024-10-23 07:11:48 -0500

    fix

--- frontend/final/src/components/prueba2.js
+++ frontend/final/src/components/prueba2.js
@@ -1,21 +1,7 @@
 import { initDynamicPanel } from "./prueba";
 import { initDynamicTable } from "./prueba";
 
-// initDynamicPanel({
-//     totalFetches:0,
-//     buttonText: '+ Registrar Insumo',
-//     panelTitle: 'Agregar Nuevo Insumo',
-//     submitButtonText: 'Registrar Insumo',
-//     apiUrl: 'http://localhost:8080/api/insumo',
-//     fields: [
-//         { label: 'Código Interno', id: 'codigoInterno', type: 'text', apiKey: 'codigo_interno' },
-//         { label: 'Nombre', id: 'nombre', type: 'text', apiKey: 'nombre' },
-//         { label: 'Precio', id: 'productPrice', type: 'number', apiKey: 'precio_unitario' },
-//         { label: 'Stock', id: 'stock', type: 'number', apiKey: 'stock' },
-//         { label: 'Stock Mínimo', id: 'stockMinimo', type: 'number', apiKey: 'stock_minimo' },
-//         { label: 'Stock Máximo', id: 'stockMaximo', type: 'number', apiKey: 'stock_maximo' }
-//     ]
-// });
+
 
 // Para otra tabla, simplemente cambia los parámetros
 initDynamicPanel({
@@ -35,21 +21,6 @@ initDynamicPanel({
         // Otros campos según sea necesario
     ]
 });
-
-initDynamicTable({
-    apiUrl: 'http://localhost:8080/api/insumo',
-    fields: [
-        { label: 'ID', id: 'id', type: 'text', apiKey: 'id' },
-        { label: 'Nombre', id: 'nombre', type: 'text', apiKey: 'nombre' },
-        { label: 'Código Interno', id: 'codigo', type: 'text', apiKey: 'codigo_interno' },
-        { label: 'Precio', id: 'precio', type: 'number', apiKey: 'precio_unitario' },
-        { label: 'Stock', id: 'stock', type: 'number', apiKey: 'stock' },
-        { label: 'Min Stock', id: 'stockMinimo', type: 'number', apiKey: 'stock_minimo' },
-        { label: 'Max Stock', id: 'stockMaximo', type: 'number', apiKey: 'stock_maximo' }
-    ]
-});
-
-// // Puedes inicializar otra tabla de la misma manera:
 
 export const listaCruds=[
     [{
