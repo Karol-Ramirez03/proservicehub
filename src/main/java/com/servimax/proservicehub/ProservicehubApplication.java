69f75c952b29a6070fc27fd132f77dab8ca90cb5
YefersonMauricioDiazChogo21 <yefersondiaz2001@gmail.com>
2024-10-25 13:03:49 -0500

    docs: :sparkles: merge security

--- src/main/java/com/servimax/proservicehub/ProservicehubApplication.java
+++ src/main/java/com/servimax/proservicehub/ProservicehubApplication.java
@@ -1,7 +1,11 @@
 package com.servimax.proservicehub;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProservicehubApplication {
@@ -9,5 +13,14 @@ public class ProservicehubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProservicehubApplication.class, args);
 	}
+
+	@Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers((Connector connector) -> {
+            connector.setProperty("maxHttpHeaderSize", "16384"); // Ajusta el tamaño según sea necesario
+        });
+        return factory;
+    }
 
 }
